9ba4dabd4013bf16c6b8b89d3ce13db9d221ef4f
noblecloud <git@noblecloud.io>
2021-04-26 22:30:07 -0400

    Added station and station collection class

---
+++ observer.py
@@ -0,0 +1,97 @@
+import datetime
+from typing import Union
+
+
+class Device:
+	deviceID: int
+	serialNumber: str
+	locationID: int
+	hardwareRevision: int
+	firmwareRevision: int
+	name: str
+
+	def __init__(self, data: dict):
+		self.deviceID = data['device_id']
+		self.serialNumber = data['serial_number']
+		self.locationID = data['location_id']
+		hardwareRevision = data['hardware_revision']
+		firmwareRevision = data['firmware_revision']
+		self.name = data['device_meta']['name']
+
+
+class Hub(Device):
+	deviceType = "hub"
+
+	def __init__(self, data):
+		super(Hub, self).__init__(data)
+
+
+class Observer(Device):
+	deviceType = "station"
+
+	def __init__(self, *args, **kwargs):
+		super(Observer, self).__init__(*args, **kwargs)
+
+
+
+class Station(dict):
+	_hub: Hub
+	_observers: list[Observer] = []
+
+	def __init__(self, data):
+
+		devices = data.pop('devices')
+
+		for device in devices:
+			deviceType = device['device_type']
+			if deviceType == 'HB':
+				self._hub = Hub(device)
+			if deviceType == 'ST':
+				self._observers.append(Observer(device))
+		super(Station, self).__init__({**data, 'hub': self._hub, 'observers': self._observers})
+
+	@property
+	def hub(self) -> Hub:
+		return self._hub
+
+	@property
+	def observers(self) -> list[Observer]:
+		return self._observers
+
+	def __repr__(self):
+		return self.name
+
+	def __str__(self):
+		return self.name
+
+	@property
+	def name(self):
+		return self['name']
+
+	@property
+	def stationID(self):
+		return self['station_id']
+
+	@property
+	def lastUpdate(self):
+		return datetime.datetime.fromtimestamp(self['last_modified_epoch'])
+
+	@property
+	def subscriptionID(self):
+		return list(station.deviceID for station in self._observers)
+
+
+class StationList(list[Station]):
+
+	def __init__(self, data: Union[dict, bytes]):
+		if isinstance(data, bytes):
+			from json import loads
+			data = loads(data.decode('ascii'))
+
+		if not isinstance(data, list):
+			data = data['stations']
+		super(StationList, self).__init__(Station(station) for station in data)
+
+	@property
+	def names(self) -> list[str]:
+		return list(station.name for station in self)
