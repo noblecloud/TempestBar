a6708501036be8fef6a381d9e475854729660d37
noblecloud <git@noblecloud.io>
2021-05-11 15:22:47 -0400

    Converted to rumps.MenuItem

--- ui/stationSelector.py
+++ ui/stationSelector.py
@@ -1,23 +1,69 @@
-from PySide6.QtCore import Signal, Slot
-from PySide6.QtWidgets import QComboBox
+from abc import ABC
+from typing import Callable
 
-from observer import Station, StationList
+from PySide6.QtCore import Signal
+from rumps import MenuItem
+from observer import Device, Station, StationList
 
 
-class StationSelector(QComboBox):
+# class DeviceItem(MenuItem, ABC):
+# 	_device: Device
+# 	_signal: Signal
+#
+# 	def toggle(self, sender):
+# 		self._device.enabled = self.state = not self.state
+# 		self._signal.emit(self._device.station)
+#
+# 	def __init__(self, device: Device, signal: Signal):
+# 		super(DeviceItem, self).__init__(device.name)
+# 		self._device = device
+# 		self._signal = signal
+	# self.set_callback(self.toggle)
 
-	stationSelection = Signal(Station)
+
+class StationItem(MenuItem, ABC):
+	_station: Station
+
+	def __init__(self, station: Station, callback: Callable):
+		super(StationItem, self).__init__(station.name, callback=callback)
+		self._station = station
+
+	@property
+	def state(self):
+		return self._menuitem.state()
+
+	@state.setter
+	def state(self, new_state):
+		self._menuitem.setState_(new_state)
+		self._station.enabled = bool(new_state)
+
+
+class StationSelector(MenuItem, ABC):
+	_signal: Signal
 	_devices: StationList
 
-	def __init__(self, *args, **kwargs):
-		super(StationSelector, self).__init__(*args, **kwargs)
-		self._devices = self._getDevices()
-		for device in self._devices:
-			self.addItem(device.name, device)
-		self.currentIndexChanged.connect(self.convertSelection)
-		self.activated.connect(self.convertSelection)
+	def __init__(self, signal: Signal):
+		super(StationSelector, self).__init__("Stations")
+		self._signal = signal
+		self.buildMenu()
+		true = True
+		default = self.values()[0]
+		default.state = true
+		self._signal.emit(default._station)
 
-	def _getDevices(self) -> StationList:
+	def buildMenu(self):
+		stations = self._getStations()
+		for station in stations:
+			self.add(StationItem(station, self.toggle))
+
+	def toggle(self, sender: StationItem):
+		sender.state = not sender.state
+		if sender.state:
+			print(f'emitting {sender._station.name}')
+			self._signal.emit(sender._station)
+
+	@classmethod
+	def _getStations(cls) -> StationList:
 		import urllib.request
 
 		url = 'https://swd.weatherflow.com/swd/rest/stations/'
@@ -27,10 +73,3 @@ class StationSelector(QComboBox):
 		url = "{}?token={}".format(url, token)
 		with urllib.request.urlopen(url) as response:
 			return StationList(response.read())
-
-	def currentData(self, **kwargs) -> Station:
-		return super(StationSelector, self).currentData(**kwargs)
-
-	@Slot(int)
-	def convertSelection(self, index: int):
-		self.stationSelection.emit(self.currentData())
